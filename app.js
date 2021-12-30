4f32bc851bd74edcd4e66535fe26bc0c497aef16
Ahmadkhan12345566 <arslanahmad12345566@gmail.com>
2021-12-30 12:44:14 +0500

    db and morgan setup

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const  connectDB = require('./config/db');
+const morgan = require('morgan');
+connectDB();
 
 
 //load config
 dotenv.config({path:'./config/config.env'})
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
 
 
 const PORT = process.env.PORT || 5000;
