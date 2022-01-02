cc5ab3ef569891cab932be6ab707018f935ec732
Ahmadkhan12345566 <arslanahmad12345566@gmail.com>
2022-01-02 22:53:16 +0500

    login view work

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const  connectDB = require('./config/db');
 const morgan = require('morgan');
+const expbhs = require('express-handlebars');
+
 connectDB();
 
 
-//load config
+//Load config
 dotenv.config({path:'./config/config.env'})
 
 const app = express();
 
+//Logging
 if (process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
+
+//Handlebars
+
+app.engine('.hbs',expbhs.engine({extname: '.hbs'}));
+app.set('view engine','.hbs');
+
+
+//Static folder
+app.use(express.static(path.join(__dirname,'public')))
+
+//Routes
+app.use('/',require('./routes/index'));
 
 const PORT = process.env.PORT || 5000;
 
